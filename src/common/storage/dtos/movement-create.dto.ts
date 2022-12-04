8416510d5bc0b8abd46a82af7e1eec20b0702d78
Sebastian Barrera Marin <sebastian.barrera@sofka.com.co>
2022-12-04 17:30:28 -0500

    feat: Messages for create movement DTO

--- src/common/storage/dtos/movement-create.dto.ts
+++ src/common/storage/dtos/movement-create.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import {
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { MovementInterface } from './interfaces/movement.interface';
 
 export class MovementCreateDto implements MovementInterface {
@@ -6,20 +12,34 @@ export class MovementCreateDto implements MovementInterface {
   id: string;
 
   @IsNotEmpty()
+  @IsString({
+    message: 'The `idIncome` argument must be of type string',
+  })
   idIncome: string;
 
   @IsNotEmpty()
+  @IsString({
+    message: 'The `idOutcome` argument must be of type string',
+  })
   idOutcome: string;
 
   @IsNotEmpty()
+  @IsString({
+    message: 'The `reason` argument must be of type string',
+  })
   reason: string;
 
   @IsNotEmpty()
+  @IsNumber()
   amount: number;
 
   @IsNotEmpty()
+  @IsNumber()
   fees: number;
 
   @IsOptional()
+  @IsDate({
+    message: 'The `date` argument must be of type Date',
+  })
   date: Date;
 }
