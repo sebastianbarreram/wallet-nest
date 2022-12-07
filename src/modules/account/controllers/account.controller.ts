b8bb6d4f7d1efb60d9c6b64aeeff7e1a31ea1a54
Sebastian Barrera Marin <sebastian.barrera@sofka.com.co>
2022-12-07 09:47:04 -0500

    feat: Get account movements and images

--- src/modules/account/controllers/account.controller.ts
+++ src/modules/account/controllers/account.controller.ts
@@ -2,13 +2,15 @@ import { Body, Controller, Get, Param, Put, UseGuards } from '@nestjs/common';
 import { AccountService } from '../services/account.service';
 import { AccountUpdateDto } from '../../../common/storage/dtos/account-update.dto';
 import { AuthGuard } from '../../../common/guards/auth-guard';
+import { AccountEntity } from '../../../common/storage/postgres/entities/account.entity';
+import { AccountGetByIDInterface } from '../../../common/storage/dtos/interfaces/account-get-by-id.interface';
 
 @Controller('api/account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
   @Get(':id')
-  getAccountByIdClient(@Param('id') id: string) {
+  getAccountByIdClient(@Param('id') id: string): Promise<AccountEntity> {
     return this.accountService.getAccountByIdClient(id);
   }
 
@@ -17,7 +19,19 @@ export class AccountController {
   updateAccountByIdClient(
     @Param('id') id: string,
     @Body() updateAccount: AccountUpdateDto,
-  ) {
+  ): Promise<AccountEntity> {
     return this.accountService.updateAccount(id, updateAccount);
+  }
+
+  @Get('images/:id')
+  getMovementPhotosByAccountId(
+    @Param('id') id: string,
+  ): Promise<AccountGetByIDInterface[]> {
+    return this.accountService.getMovementPhotosByAccountId(id);
+  }
+
+  @Get('movements/:id')
+  getMovementsByAccountId(@Param('id') id: string) {
+    return this.accountService.getMovementsByAccountId(id);
   }
 }
--- src/modules/account/services/account.service.ts
+++ src/modules/account/services/account.service.ts
@@ -3,6 +3,8 @@ import { Repository } from 'typeorm';
 import { AccountEntity } from '../../../common/storage/postgres/entities/account.entity';
 import { AccountUpdateDto } from '../../../common/storage/dtos/account-update.dto';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AccountGetByIDInterface } from '../../../common/storage/dtos/interfaces/account-get-by-id.interface';
+import { MovementInterface } from '../../../common/storage/dtos/interfaces/movement.interface';
 
 @Injectable()
 export class AccountService {
@@ -16,7 +18,10 @@ export class AccountService {
       where: {
         idClient: id,
       },
-      relations: { movementsIncome: true, movementsOutcome: true },
+      relations: {
+        movementsIncome: true,
+        movementsOutcome: true,
+      },
     });
     return Promise.resolve(account);
   }
@@ -28,5 +33,60 @@ export class AccountService {
     account.updatedAt = new Date(Date.now());
     const newAccount = await this.accountRepository.save(account);
     return Promise.resolve(newAccount);
+  }
+
+  async getAccountById(id: string): Promise<AccountGetByIDInterface> {
+    const account = await this.accountRepository.findOne({
+      where: {
+        id: id,
+      },
+      relations: { client: true },
+    });
+    const accountGet: AccountGetByIDInterface = {
+      id: id,
+      photo: account.client.photo,
+    };
+    return Promise.resolve(accountGet);
+  }
+
+  async getMovementPhotosByAccountId(
+    id: string,
+  ): Promise<AccountGetByIDInterface[]> {
+    const movements = await this.getMovementsByAccountId(id);
+    const idAccountOutcome = new Set(
+      movements.map((element) => element.idOutcome),
+    );
+    const idAccountIncome = new Set(
+      movements.map((element) => element.idIncome),
+    );
+    for (const item of idAccountOutcome.values()) {
+      idAccountIncome.add(item);
+    }
+    const images: AccountGetByIDInterface[] = [];
+    for (const id of idAccountIncome) {
+      const accountPhoto = await this.getAccountById(id);
+      images.push(accountPhoto);
+    }
+    return images;
+  }
+
+  async getMovementsByAccountId(id: string) {
+    const account = await this.accountRepository.findOne({
+      where: {
+        id: id,
+      },
+      relations: {
+        movementsIncome: true,
+        movementsOutcome: true,
+      },
+    });
+    const incomes = account.movementsIncome;
+    const outcomes = account.movementsOutcome;
+    const ids = new Set(incomes.map((element) => element.id));
+    const transactions: MovementInterface[] = [
+      ...incomes,
+      ...outcomes.filter((item) => !ids.has(item.id)),
+    ];
+    return transactions;
   }
 }
